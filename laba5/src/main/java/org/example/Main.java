d82cc8c09eb6532ae1239d146526a5803e345e46
Ivan <sutarevivan@gmail.com>
2024-01-17 08:51:03 +0400

    master_kommit

--- laba5/src/main/java/org/example/Main.java
+++ laba5/src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Commit 4");
-=======
-        System.out.println("lalala");
->>>>>>> new_branch
+        System.out.println("Master");
     }
 }
